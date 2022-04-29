99edf36cca0e8e272b309a5660cbc0206f5eef29
KumaranShivam5 <kumaranshivam57@gmail.com>
2022-04-29 12:15:01 +0530

    Auto commit Fri Apr 29 12:15:01 IST 2022

--- OneVsRestLightGBMWithCustomizedLoss.py
+++ OneVsRestLightGBMWithCustomizedLoss.py
@@ -48,20 +48,20 @@ class OneVsRestLightGBMWithCustomizedLoss:
                 val = lgb.Dataset(X_val, y_val, init_score=np.full_like(y_val, init_score_value,),
                                   reference=fit)
 
-                estimator = lgb.train(params={},
+                estimator = lgb.train(params=fit_params['fit_fit_params'],
                                       train_set=fit,
                                       valid_sets=(fit, val),
                                       valid_names=('fit', 'val'),
                                       early_stopping_rounds=10,
                                       fobj=self.loss.lgb_obj,
                                       feval=self.loss.lgb_eval,
-                                      verbose_eval=10)
+                                      verbose_eval=True)
             else:
-                estimator = lgb.train(params={},
+                estimator = lgb.train(params=fit_params['fit_fit_params'],
                                       train_set=fit,
                                       fobj=self.loss.lgb_obj,
                                       feval=self.loss.lgb_eval,
-                                      verbose_eval=10)
+                                      verbose_eval=True)
 
         return estimator, init_score_value
 
