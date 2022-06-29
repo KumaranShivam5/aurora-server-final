5ae9c7deb6de4ebcb22fae48c5178378576ab692
KumaranShivam5 <kumaranshivam57@gmail.com>
2022-06-30 00:10:01 +0530

    Auto commit Thu Jun 30 00:10:01 IST 2022

--- phd_4most/utilities_v3.py
+++ phd_4most/utilities_v3.py
@@ -27,7 +27,7 @@ from imblearn.over_sampling import SMOTE
 
 
 
-def train_model_loo(arr):
+def train_model_leave_one_out(arr):
     model , x ,y , index = arr
     train_ix , test_ix = index[0] , index[1]
     x_train , x_test = x.loc[train_ix , : ] , x.loc[test_ix, :]
@@ -41,10 +41,8 @@ def train_model_loo(arr):
     clf.fit(x_train_up , y_train_up)
     return [clf.predict(x_test)[0], y_test , clf.predict_proba(x_test)]
 
-from IPython.display import display
-def train_model_kfold(arr):
+def train_model_k_fold(arr):
     model , x ,y , index = arr
-    display(x)
     train_ix , test_ix  , = index[0] , index[1]
     x_train , x_test = x.iloc[train_ix , : ] , x.iloc[test_ix, :]
     # test_names = names.loc[test_ix]
@@ -56,7 +54,7 @@ def train_model_kfold(arr):
     x_train_up , y_train_up = oversampling(oversampler , x_train, y_train)
     x_train_up = x_train_up.replace(np.nan , -100)
     clf = model
-    clf.fit(x_train , y_train)
+    clf.fit(x_train_up , y_train_up)
     df = pd.DataFrame({
         'name' : x_test.index.to_list() , 
         'true_class' : y_test , 
@@ -88,13 +86,13 @@ def cumulative_cross_validation(x ,y , model , k_fold=-1 , save_result_filename 
         num_cores = mp.cpu_count()
         with mp.Pool(int(num_cores)) as pool:
             if(k_fold==-1):
-                res = pool.map(train_model_loo , arr) 
+                res = pool.map(train_model_leave_one_out , arr) 
             else: 
-                res = pool.map(train_model_kfold , arr) 
+                res = pool.map(train_model_k_fold , arr) 
     else:
         res = []
         for a in tqdm(arr):
-            res.append(train_model_kfold(a))
+            res.append(train_model_k_fold(a))
 
     if k_fold==-1 :
         res_df  = pd.DataFrame({
@@ -114,6 +112,56 @@ def cumulative_cross_validation(x ,y , model , k_fold=-1 , save_result_filename 
     return res_df
 
 
+def get_score(y_true , y_pred , pred_prob , confidance=0 , sc_average = 'weighted'):
+    y_true = y_true[pred_prob>confidance]
+    y_pred = y_pred[pred_prob > confidance]
+    pred_prob = pred_prob[pred_prob>confidance]
+    y_total = rdata['true_class'].value_counts().to_dict()
+    #print(y_total)
+    pred_min = rdata['pred_prob'].min()
+    pred_prob = rdata['pred_prob']
+    rdata= rdata[rdata['pred_prob']>confidance]
+    y_true = rdata['true_class']
+    y_pred = rdata['pred_class']
+    y_true_count = y_true.value_counts().to_dict()
+    y_pred_count = y_pred.value_counts().to_dict()
+    #print(y_pred_count)
+    #labels = y_true.unique()
+    #rint(labels)
+    labels = np.sort(y_true.unique())
+    xticks , yticks = [] , []
+    for l in labels:
+        try:
+            yticks.append(f'{l}\n{y_true_count[l]}/{y_total[l]}')
+            #xticks.append(f'{l}\n{y_pred_count[l]}')
+        except : 
+            yticks.append(f'{l}\n{0}/{y_total[l]}')
+            #xticks.append(f'{l}\n{0}')
+    #print(labels)
+    from sklearn.metrics import accuracy_score , balanced_accuracy_score , precision_score , f1_score , recall_score , roc_auc_score , matthews_corrcoef , confusion_matrix
+    cm =  confusion_matrix(y_true , y_pred , normalize='true' , labels = labels)
+    #f1 = recall_score(y_true , y_pred , average=None , )
+    num_src = y_pred.value_counts().to_frame()
+    score_dict = {
+        'classes' : labels ,
+        'num_src' : num_src , 
+        'avg_scores': {
+            'balanced_accuracy' : balanced_accuracy_score(y_true , y_pred ) , 
+            'accuracy' : accuracy_score(y_true , y_pred , ) , 
+            'precision' : precision_score(y_true , y_pred , average=sc_average) , 
+            'recall' : recall_score(y_true , y_pred , average=sc_average) , 
+            'f1' : f1_score(y_true , y_pred , average=sc_average)
+        } , 
+        #'roc_auc' : roc_auc_score(y_true , pred_prob , average = 'micro' , multi_class='ovr') ,
+        'mcc' : matthews_corrcoef(y_true , y_pred),
+        'class_scores' : pd.DataFrame({
+            'class' : labels , 
+            'recall_score' : recall_score(y_true , y_pred , average=None , ) , 
+            'precision_score' : precision_score(y_true , y_pred , average=None , ) ,
+            'f1_score' : f1_score(y_true , y_pred , average=None , )
+        }).sort_values(by='class').set_index('class') , 
+    }
+    return score_dict
 
 
 class make_model():
