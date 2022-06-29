a64769040b0ec3bce36a1b89c9c27fe7d994b9c6
KumaranShivam5 <kumaranshivam57@gmail.com>
2022-06-29 23:40:01 +0530

    Auto commit Wed Jun 29 23:40:01 IST 2022

--- phd_4most/utilities_v3.py
+++ phd_4most/utilities_v3.py
@@ -46,10 +46,7 @@ def train_model_loo(arr):
     x_train_up , y_train_up = oversampling(oversampler , x_train, y_train)
     x_train_up = x_train_up.replace(np.nan , -100)
     #x_test_up = x_test_up.replace(np.nan , -100)
-    if(type(model)==str):
-        clf = model_dict[model]
-    else:
-        clf = model
+    clf = model
     clf.fit(x_train_up , y_train_up)
     return [clf.predict(x_test)[0], y_test , clf.predict_proba(x_test)]
 
@@ -65,10 +62,7 @@ def train_model_kfold(arr):
     oversampler  = SMOTE(k_neighbors=4)
     x_train_up , y_train_up = oversampling(oversampler , x_train, y_train)
     x_train_up = x_train_up.replace(np.nan , -100)
-    if(type(model['model'])==str):
-        clf = model_dict[model['model']]
-    else:
-        clf = model['model']
+    clf = model
     clf.fit(x_train , y_train)
     df = pd.DataFrame({
         'name' : test_names , 
@@ -82,16 +76,17 @@ def train_model_kfold(arr):
     df = pd.merge(df , mem_table , left_index=True , right_index=True)
     return df
 
-def cumulative_cross_validation(x ,y , model , k=-1 , return_dict  = ret_dict , save_df = '' , multiprocessing = 1 ):
-    classes = y.unique()
-    x = x.drop(columns=['class'])
 
-    if k==-1:
+
+from sklearn.model_selection import LeaveOneOut , StratifiedKFold 
+def cumulative_cross_validation(x ,y , model , k_fold=-1 , save_result_filename = '' , multiprocessing = 1 ):
+
+    if k_fold==-1:
         print('[INFO] Doing LeaveOneOut cross-validation')
         cv = LeaveOneOut()
     else:
-        print(f'Doing {k} fold cross-validation')
-        cv = StratifiedKFold(k)# KFold(k) 
+        print(f'Doing {k_fold} fold cross-validation')
+        cv = StratifiedKFold(k_fold)# KFold(k) 
     model = model
     index = [(t,i) for t,i in cv.split(x,y)]
     arr = list(zip([model]*len(index) , [x]*len(index) , [y]*len(index) , index ))
@@ -99,7 +94,7 @@ def cumulative_cross_validation(x ,y , model , k=-1 , return_dict  = ret_dict , 
         import multiprocessing as mp 
         num_cores = mp.cpu_count()
         with mp.Pool(int(num_cores)) as pool:
-            if(k==-1):
+            if(k_fold==-1):
                 res = pool.map(train_model_loo , arr) 
             else: 
                 res = pool.map(train_model_kfold , arr) 
@@ -108,59 +103,20 @@ def cumulative_cross_validation(x ,y , model , k=-1 , return_dict  = ret_dict , 
         for a in tqdm(arr):
             res.append(train_model_kfold(a))
 
-    if k==-1 :
+    if k_fold==-1 :
         res_df  = pd.DataFrame({
             'true_class' : [el[1].iloc[0] for el in res] , 
             'pred_class' : [el[0] for el in res], 
             'pred_prob' : [np.amax(el[2] )for el in res]
         })
-        if(save_df):
-            res_df.to_csv(f'validation_res/{d_name}_{m_name}_loo.csv')
+        if(save_result_filename):
+            res_df.to_csv(f'{save_result_filename}')
     else:
         res_df = pd.concat(res, axis=0)
         #display(res_df)
-        if(save_df):
-            res_df.to_csv(f'{save_df}')
-    acc_sc = accuracy_score(res_df['true_class'] , res_df['pred_class'])
-
-    print(f'[RESULT] Overall Accuracy : {acc_sc :.2f}')
-    ret = {}
-    if(return_dict['prob_table']):
-        print("[INFO] Validation probability table is available as ['prob_table'] in response")
-        ret['prob_table'] = res_df
-    if(return_dict['clf']):
-        if(type(model['model'])==str):
-            clf = model_dict[model['model']]
-        else: 
-            clf = model['model']
-        print(f'>>> Training the final classifier {clf}')
-        #print('>>> Classifier is : ' , clf)
-        clf.fit(x,y)
-        ret['clf'] = clf
-        print("[DONE] Classifier is trained | acces via ['clf'] in the response")
-
-    if(return_dict['acc']): ret['acc'] = accuracy_score(res_df['true_class'] , res_df['pred_class'])
-    if(return_dict['precision']): ret['precision'] = precision_score(res_df['true_class'] , res_df['pred_class'] , average='macro')
-    if(return_dict['recall']): ret['recall'] = recall_score(res_df['true_class'] , res_df['pred_class'],  average='macro')
-    
-    if(return_dict['pr_score']):
-        pres = np.diag(confusion_matrix(res_df['true_class'] , res_df['pred_class'], normalize='true' , labels=y.unique()))
-        recall = np.diag(confusion_matrix(res_df['true_class'] , res_df['pred_class'], normalize='pred' , labels=y.unique()))
-        pr = pd.DataFrame({
-            'class' : y.unique() , 
-            'precision' : pres , 
-            'recall' : recall
-        })
-        ret['pr_score'] = pr
-    # mem_table = pd.concat([el[1] for el in res_a])#.reset_index(drop=True)
-    # display(mem_table)
-    # #mem_table.insert(0 , 'name' , x_index)
-    # mem_table = mem_table.set_index('name')
-    mem_columns = [f'prob_{el}' for el in classes]
-    if(return_dict['roc_auc_score']):
-        ra_score = roc_auc_score(res_df['true_class'] , res_df[mem_columns] , multi_class='ovr' , average = 'weighted') 
-        ret['roc-auc'] = ra_score
-    return ret
+        if(save_result_filename):
+            res_df.to_csv(f'{save_result_filename}')
+    return res_df
 
 
 
@@ -171,18 +127,17 @@ class make_model():
         self.clf = clf 
         self.x = x 
         self.y = y 
+        self.validation_prediction = 'validation predictions are not stored'
         
-    def validate(self , fname= '' , k=10 , normalize_prob=0 , score_average = 'macro'):
+    def validate(self , fname= '' , k=10 , normalize_prob=0 , score_average = 'macro' , save_predictions = ''):
         from utilities import simple_cv
         #self.weight = self.calc_weight(self.gamma ,self.y)
-        res = simple_cv(self.x,self.y , model=self.clf , k=k , normalize_prob=normalize_prob , score_average = score_average)
-        res['gamma'] = self.gamma 
-        #res['class_weight'] = calc_weight(slef)
-        print(res['class_scores'].to_markdown())
-        self.result = res
-        if(fname):
-            import joblib
-            joblib.dump(res , fname)
+        validation_predictions = simple_cv(self.x,self.y , model=self.clf , k=k , normalize_prob=normalize_prob , score_average = score_average)
+        if(save_predictions):
+            self.validation_prediction = validation_predictions
+        # if(fname):
+        #     import joblib
+        #     joblib.dump(res , fname)
         return self
     
     def train(self):
