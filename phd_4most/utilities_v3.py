da1fdfc671a826defb69c6063fbb3aea5582cdd4
KumaranShivam5 <kumaranshivam57@gmail.com>
2022-06-30 00:40:01 +0530

    Auto commit Thu Jun 30 00:40:01 IST 2022

--- phd_4most/utilities_v3.py
+++ phd_4most/utilities_v3.py
@@ -7,21 +7,10 @@ from sklearn.ensemble import RandomForestClassifier , GradientBoostingClassifier
 from sklearn.model_selection import LeaveOneOut , StratifiedKFold 
 from imblearn.over_sampling import SMOTE
 
-model_dict = {
-    'RF' : RandomForestClassifier() , 
-    'RF_mod' : RandomForestClassifier(
-        n_estimators = 500 , 
-    ) , 
-    'GB' : GradientBoostingClassifier() , 
-}
+
+
 def oversampling(method, X_train, y_train):
-    # oversampling training dataset to mitigate for the imbalanced TD
-    # default = SMOTE
-    exnum = 9282948832
-    # X_train.replace(np.nan, exnum, inplace=True)
     X_res, y_res = method.fit_resample(X_train, y_train)
-    # res = X_res.values
-    # X_res[:] = res
     return X_res, y_res
 
 
@@ -30,28 +19,22 @@ def oversampling(method, X_train, y_train):
 
 def train_model_leave_one_out(arr):
     model , x ,y , index = arr
-    train_ix , test_ix = index[0] , index[1]
-    x_train , x_test = x.loc[train_ix , : ] , x.loc[test_ix, :]
-    y_train , y_test = y.loc[train_ix] , y.loc[test_ix]
+    train_index , test_index = index[0] , index[1]
+    x_train , x_test = x.loc[train_index , : ] , x.loc[test_index, :]
+    y_train , y_test = y.loc[train_index] , y.loc[test_index]
     oversampler  = SMOTE(k_neighbors=4)
     x_train_up , y_train_up = oversampling(oversampler , x_train, y_train)
-    # x_train_up = x_train_up.replace(np.nan , -100)
     clf = model
     clf.fit(x_train_up , y_train_up)
     return [clf.predict(x_test)[0], y_test , clf.predict_proba(x_test)]
 
 def train_model_k_fold(arr):
     model , x ,y , index = arr
-    train_ix , test_ix  , = index[0] , index[1]
-    x_train , x_test = x.iloc[train_ix , : ] , x.iloc[test_ix, :]
-    # test_names = names.loc[test_ix]
-    y_train , y_test = y.iloc[train_ix] , y.iloc[test_ix]
-    # x_test = x_test.reset_index(drop=True)
-    # y_test = y_test.reset_index(drop=True)
-    # test_names = test_names.reset_index(drop=True)
+    train_index , test_index  , = index[0] , index[1]
+    x_train , x_test = x.iloc[train_index , : ] , x.iloc[test_index, :]
+    y_train , y_test = y.iloc[train_index] , y.iloc[test_index]
     oversampler  = SMOTE(k_neighbors=4)
     x_train_up , y_train_up = oversampling(oversampler , x_train, y_train)
-    # x_train_up = x_train_up.replace(np.nan , -100)
     clf = model
     clf.fit(x_train_up , y_train_up)
     df = pd.DataFrame({
@@ -60,10 +43,10 @@ def train_model_k_fold(arr):
         'pred_class' : clf.predict(x_test) , 
         'pred_prob' : [np.amax(el) for el in clf.predict_proba(x_test)]
     }).set_index('name')
-    # mem_table = pd.DataFrame(clf.predict_proba(x_test) , columns=[f'prob_{el}' for el in clf.classes_])
-    # mem_table.insert(0 , 'name' , test_names)
-    # mem_table = mem_table.set_index('name')
-    # df = pd.merge(df , mem_table , left_index=True , right_index=True)
+    membership_table = pd.DataFrame(clf.predict_proba(x_test) , columns=[f'prob_{el}' for el in clf.classes_])
+    membership_table.insert(0 , 'name' , x_test.index.to_list())
+    membership_table = membership_table.set_index('name')
+    df = pd.merge(df , membership_table , left_index=True , right_index=True)
     return df
 
 
@@ -78,36 +61,35 @@ def cumulative_cross_validation(x ,y , model , k_fold=-1 , save_result_filename 
         cv = StratifiedKFold(k_fold)# KFold(k) 
     model = model
     index = [(t,i) for t,i in cv.split(x,y)]
-    arr = list(zip([model]*len(index) , [x]*len(index) , [y]*len(index) , index ))
+    zipped_arr = list(zip([model]*len(index) , [x]*len(index) , [y]*len(index) , index ))
     if(multiprocessing):
         import multiprocessing as mp 
         num_cores = mp.cpu_count()
         with mp.Pool(int(num_cores)) as pool:
             if(k_fold==-1):
-                res = pool.map(train_model_leave_one_out , arr) 
+                result = pool.map(train_model_leave_one_out , zipped_arr) 
             else: 
-                res = pool.map(train_model_k_fold , arr) 
+                result = pool.map(train_model_k_fold , zipped_arr) 
     else:
-        res = []
-        for a in tqdm(arr):
-            res.append(train_model_k_fold(a))
+        result = []
+        for a in tqdm(zipped_arr):
+            result.append(train_model_k_fold(a))
 
     if k_fold==-1 :
-        res_df  = pd.DataFrame({
-            'name' : res.index.to_list() , 
-            'true_class' : [el[1].iloc[0] for el in res] , 
-            'pred_class' : [el[0] for el in res], 
-            'pred_prob' : [np.amax(el[2] )for el in res]
+        result_df  = pd.DataFrame({
+            'name' : result.index.to_list() , 
+            'true_class' : [el[1].iloc[0] for el in result] , 
+            'pred_class' : [el[0] for el in result], 
+            'pred_prob' : [np.amax(el[2] )for el in result]
         }).set_index('name')
         if(save_result_filename):
-            res_df.to_csv(f'{save_result_filename}')
+            result_df.to_csv(f'{save_result_filename}')
     else:
         print('doing k fold in else')
-        res_df = pd.concat(res, axis=0)
-        #display(res_df)
+        result_df = pd.concat(result, axis=0)
         if(save_result_filename):
-            res_df.to_csv(f'{save_result_filename}')
-    return res_df
+            result_df.to_csv(f'{save_result_filename}')
+    return result_df
 
 
 def get_score(pred_table  , confidance=0 , score_average_type = 'weighted'):
